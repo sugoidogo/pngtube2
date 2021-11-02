575827fc456a063cece978bf4ff1471ad063d234
josephsmendoza <josephsmendoza@gmail.com>
2021-11-02 23:49:55 +0000

    alpha 2

--- pngtube.py
+++ pngtube.py
@@ -1,46 +1,66 @@
 from http.server import SimpleHTTPRequestHandler, HTTPServer
-import sounddevice
-import numpy
-import os
+from pathlib import Path
+import sounddevice, numpy, os, json, shutil
 
 class pngtube(SimpleHTTPRequestHandler):
     protocol_version='HTTP/1.1'
     def do_POST(self):
-        from pathlib import Path
         path=Path.cwd().joinpath(self.path[1:]) # attempt to create absolute path under web root
-        if not path.is_relative_to(Path.cwd()): # if the resulting path is not under web root
-            self.send_error(403,explain='requested resource is outside web root') # never write to files outside of web root
+        print(path)
+        parent=path.parent
+        print(parent)
+        if not parent.parent.is_relative_to(Path.cwd()): # if the resulting path is not a sub-path in web root
+            self.send_error(405) # never write to files outside of web root directories
             return
         try: # attempt to write data to storage
-            path.parent.mkdir(parents=True,exist_ok=True)
+            parent.mkdir(parents=True,exist_ok=True) # make sure parent exists
+            size=int(self.headers['Content-Length'])
+            if shutil.disk_usage(parent).free < size: # if there's not enough space
+                return self.send_error(507)           # error out
             with path.open('wb') as file:
-                file.write(self.rfile.read(int(self.headers['Content-Length'])))
+                file.write(self.rfile.read(size))
         except Exception as e: # writing data failed
             self.send_error(500,explain=str(e))
         else: # writing data succeeded
             self.send_response(200)
     def do_GET(self):
-        if(self.headers['Transfer-Encoding']!='chunked'):
+        if not self.headers['Audio']:
             return SimpleHTTPRequestHandler.do_GET(self)
-        self.send_response(200)
-        self.send_header('Transfer-Encoding','chunked')
-        self.send_header('Content-Type','text/plain')
-        self.end_headers()
-        def send(message):
-            try:
+        if self.headers['Audio']=='list':
+            response=json.dumps(sounddevice.query_devices(kind='input'))
+            if response.startswith('{'):
+                response="["+response+"]"
+            response+="\n"
+            self.send_response(200)
+            self.send_header('Content-Type','text/json')
+            self.send_header('Content-Length',str(len(response)))
+            self.end_headers()
+            self.wfile.write(response.encode())
+            return
+        device=self.headers['Audio']
+        try:
+            sounddevice.check_input_settings(device=device)
+            self.send_response(200)
+            self.send_header('Transfer-Encoding','chunked')
+            self.send_header('Content-Type','text/plain')
+            self.end_headers()
+        except Exception as e:
+            self.send_error(500,explain=str(e))
+        def audioLoop(indata, frames, time, status):
+            def send(message):
                 self.wfile.write(str(message).encode())
+            message=str(numpy.linalg.norm(indata)*10)
+            try:
+                send(len(message))
+                send('\r\n')
+                send(message)
+                send('\r\n')
+                self.wfile.flush()
             except:
                 os._exit(0)
-        def loop(indata, frames, time, status):
-            num=str(numpy.linalg.norm(indata)*10)
-            send(len(num))
-            send("\r\n")
-            send(num)
-            send("\r\n")
-            self.wfile.flush()
         if(os.fork()==0):
-            with sounddevice.InputStream(callback=loop):
+            with sounddevice.InputStream(device=device,callback=audioLoop):
                 while True:
                     sounddevice.sleep(1)
 
-HTTPServer(('localhost',6000), pngtube).serve_forever()
+HTTPServer(('localhost',5000), pngtube).serve_forever()
